06f53199525cc76cfc8c9e30fa6c3745c1900299
earto <eltonline@outlook.com>
2026-06-28 19:04:47 +1200

    Update const.py

--- custom_components/default_config_disabler/const.py
+++ custom_components/default_config_disabler/const.py
@@ -2,7 +2,7 @@
 
 from typing import Final
 
-DOMAIN: Final = "default_config_disabler"
-NAME: Final = "Default Config Disabler"
+DOMAIN: Final = "default_config_manager"
+NAME: Final = "Default Config Manager"
 
 CONF_COMPONENTS_TO_DISABLE: Final = "components_to_disable"
