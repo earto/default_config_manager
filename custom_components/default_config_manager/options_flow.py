45333f7ccb8d35ba2dd249de332ae31db04230fb
earto <eltonline@outlook.com>
2026-06-30 12:11:13 +1200

    Update options_flow.py

--- custom_components/default_config_manager/options_flow.py
+++ custom_components/default_config_manager/options_flow.py
@@ -12,11 +12,12 @@ from .const import (
     DOMAIN,
     CONF_ADVANCED_MODE,
     CONF_COMPONENTS_TO_DISABLE,
+    LOGGER_PREFIX,
 )
 from .helpers import get_static_integrations
 
 import logging
-_LOGGER = logging.getLogger("DCM")
+_LOGGER = logging.getLogger(LOGGER_PREFIX)
 
 
 class DefaultConfigManagerOptionsFlow(config_entries.OptionsFlow):
@@ -24,28 +25,25 @@ class DefaultConfigManagerOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         self.config_entry = config_entry
-        _LOGGER.debug(
-            "DCM: OptionsFlow __init__ called for entry_id=%s",
-            config_entry.entry_id,
-        )
+        _LOGGER.debug("OptionsFlow __init__ called for entry_id=%s", config_entry.entry_id)
 
     async def async_step_init(self, user_input=None):
-        _LOGGER.debug("DCM: OptionsFlow async_step_init called, user_input=%s", user_input)
+        _LOGGER.debug("OptionsFlow async_step_init called, user_input=%s", user_input)
         return await self.async_step_user(user_input)
 
     async def async_step_user(self, user_input: dict[str, Any] | None = None):
         """Options form."""
-        _LOGGER.debug("DCM: OptionsFlow async_step_user called, user_input=%s", user_input)
+        _LOGGER.debug("OptionsFlow async_step_user called, user_input=%s", user_input)
 
         if user_input is not None:
-            _LOGGER.debug("DCM: OptionsFlow creating entry with data=%s", user_input)
+            _LOGGER.debug("OptionsFlow creating entry with data=%s", user_input)
             return self.async_create_entry(
                 title="Options",
                 data=user_input,
             )
 
         static_integrations = await get_static_integrations(self.config_entry.hass)
-        _LOGGER.debug("DCM: static_integrations=%s", static_integrations)
+        _LOGGER.debug("static_integrations=%s", static_integrations)
 
         schema = vol.Schema(
             {
