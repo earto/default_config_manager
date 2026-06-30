dd5b2887597f5fa3ef1bfde4bcf27e11475e7a34
earto <eltonline@outlook.com>
2026-06-30 12:09:08 +1200

    Update const.py

--- custom_components/default_config_manager/const.py
+++ custom_components/default_config_manager/const.py
@@ -2,7 +2,7 @@
 
 DOMAIN = "default_config_manager"
 NAME = "Default Config Manager"
-LOGGER_PREFIX = "Default Config Manager"
+LOGGER_PREFIX = "default_config_manager"
 
 CONF_ADVANCED_MODE = "advanced_mode"
 CONF_COMPONENTS_TO_DISABLE = "components_to_disable"
