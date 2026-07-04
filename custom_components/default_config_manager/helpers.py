97b5177379da760c29fc8fa3b14144dedd056d4e
earto <eltonline@outlook.com>
2026-07-04 23:49:28 +1200

    Refactor default config loading in helpers.py
    Removed unused function and streamlined default config loading.

--- custom_components/default_config_manager/helpers.py
+++ custom_components/default_config_manager/helpers.py
@@ -23,23 +23,6 @@ async def get_static_integrations(hass: HomeAssistant) -> List[str]:
         _LOGGER.error("Failed to load default_config manifest: %s", err)
         return []
 
-    return await hass.async_add_executor_job(
-        _load_default_config_dependencies,
-        components_path,
-    )
-
-
-async def get_conditional_integrations(hass: HomeAssistant) -> List[str]:
-    """Return conditional integrations enabled indirectly by default_config."""
-    static_components = await get_static_integrations(hass)
-    running_components = list(hass.config.components)
-
-    return sorted(
-        component
-        for component in running_components
-        if component not in static_components
-    )
-
 
 async def get_default_config_version(hass: HomeAssistant) -> str:
     """Return the Home Assistant Core version."""
