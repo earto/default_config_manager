3fcf6eeaee1d9917afe907b7714baa768f7a198e
earto <eltonline@outlook.com>
2026-07-04 13:10:01 +1200

    Update mode display descriptions in const.py

--- custom_components/default_config_manager/const.py
+++ custom_components/default_config_manager/const.py
@@ -11,7 +11,7 @@ MODE_2 = 2  # Basic (Managed)
 MODE_3 = 3  # Advanced (Managed)
 
 MODE_DISPLAY = {
-    MODE_1: "Unmanaged (Config File loads default_config)",
-    MODE_2: "Managed (Read Only)",
+    MODE_1: "Unmanaged (Configuration File)",
+    MODE_2: "Managed (Basic)",
     MODE_3: "Managed (Advanced)",
 }
