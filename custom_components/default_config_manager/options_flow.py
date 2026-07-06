bca6342d19ae848e2f3855e2ab936c7788a26841
earto <eltonline@outlook.com>
2026-07-06 23:06:07 +1200

    Update options_flow.py

--- custom_components/default_config_manager/options_flow.py
+++ custom_components/default_config_manager/options_flow.py
@@ -130,6 +130,21 @@ class DefaultConfigManagerOptionsFlow(config_entries.OptionsFlow):
                     }
                 )
             ),
+            vol.Required("integrations_section"): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            "integration_list",
+                            default=active_components_text,
+                        ): TextSelector(
+                            TextSelectorConfig(
+                                multiple=False,
+                                multiline=True,
+                            )
+                        ),
+                    }
+                )
+            ),
         }
 
         return self.async_show_form(
