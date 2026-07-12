d2e8419aca170c2faff839f56843f8d5a0aceacb
earto <eltonline@outlook.com>
2026-07-12 17:56:34 +1200

    Update const.py

--- custom_components/default_config_manager/const.py
+++ custom_components/default_config_manager/const.py
@@ -6,12 +6,14 @@ NAME = "Default Config Manager"
 CONF_ADVANCED_MODE = "advanced_mode"
 CONF_COMPONENTS_TO_DISABLE = "components_to_disable"
 
-MODE_1 = 1  # Basic (Config File)
-MODE_2 = 2  # Basic (Managed)
-MODE_3 = 3  # Advanced (Managed)
+MODE_0 = 0  # Unmanaged (Missing YAML)
+MODE_1 = 1  # Unmanaged (Factory YAML wins)
+MODE_2 = 2  # Managed (Basic)
+MODE_3 = 3  # Managed (Advanced)
 
 MODE_DISPLAY = {
-    MODE_1: "Unmanaged (Configuration File)",
+    MODE_0: "Unmanaged (Missing from configuration.yaml)",
+    MODE_1: "Unmanaged (Factory default_config active)",
     MODE_2: "Managed (Basic)",
     MODE_3: "Managed (Advanced)",
 }
