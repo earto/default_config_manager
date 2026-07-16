41bdcad44ac041a6925732941c57afeb0eb22483
earto <eltonline@outlook.com>
2026-07-17 00:43:42 +1200

    Update MODE_DISPLAY description for MODE_0

--- custom_components/default_config_manager/const.py
+++ custom_components/default_config_manager/const.py
@@ -12,7 +12,7 @@ MODE_2 = 2  # Managed (Basic)
 MODE_3 = 3  # Managed (Advanced)
 
 MODE_DISPLAY = {
-    MODE_0: "No Default Config (Missing from config file)",
+    MODE_0: "None (Missing from config file)",
     MODE_1: "Unmanaged (Factory default_config active)",
     MODE_2: "Managed (Read Only)",
     MODE_3: "Managed (Advanced)",
